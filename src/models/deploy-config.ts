c53ca1f9ec5aed28f500813cd1f9fca4e1ce814d
Jordan Ribbink <jribbink@telus.net>
2022-02-14 12:43:21 -0800

    Fix read config

--- src/models/deploy-config.ts
+++ src/models/deploy-config.ts
@@ -1,12 +1,13 @@
 import YAML from 'yaml'
 import path from 'path'
+import fs from 'fs'
 
 export class DeployConfig {
     static readonly DEPLOY_FILE_NAME = "bcd-deploy.yaml"
 
     static parse(repo_path: string): DeployConfig {
         const file_path = path.resolve(repo_path, DeployConfig.DEPLOY_FILE_NAME)
-        const yaml = YAML.parse(file_path)
+        const yaml = YAML.parse(fs.readFileSync(file_path).toString())
         console.log(yaml)
 
         const config = new DeployConfig()
