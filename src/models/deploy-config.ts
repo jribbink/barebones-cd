8efa737d0f2b0185ce71b2f774395375e24d55f0
Jordan Ribbink <jribbink@telus.net>
2022-02-14 12:36:08 -0800

    Improved deploy functionality

--- src/models/deploy-config.ts
+++ src/models/deploy-config.ts
@@ -1,8 +1,12 @@
 import YAML from 'yaml'
+import path from 'path'
 
 export class DeployConfig {
-    static parse(filename: string): DeployConfig {
-        const yaml = YAML.parse(filename)
+    static readonly DEPLOY_FILE_NAME = "bcd-deploy.yaml"
+
+    static parse(repo_path: string): DeployConfig {
+        const file_path = path.resolve(repo_path, DeployConfig.DEPLOY_FILE_NAME)
+        const yaml = YAML.parse(file_path)
         console.log(yaml)
 
         const config = new DeployConfig()
--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,10 +1,12 @@
 import { Webhooks as OctokitWebhooks } from "@octokit/webhooks";
-import { WebhookEventName } from "@octokit/webhooks-types";
+import { Repository, WebhookEventName } from "@octokit/webhooks-types";
 import { HandlerFunction, Options } from "@octokit/webhooks/dist-types/types";
 import { AppConfig } from "./app-config";
 import child_process from "child_process";
 import path from "path";
+import fs from "fs";
 import { resolveHome } from "./utils/resolve-home";
+import { DeployConfig } from "./models/deploy-config";
 
 const config = AppConfig.getInstance()
 
@@ -20,14 +22,42 @@ export class Webhooks extends OctokitWebhooks {
     addWebhooks() {
         this.addWebhook("push", (options) => {
             const payload = options.payload
-            const default_branch = options.payload.repository.default_branch
             console.log(payload)
-        
-            const ssh_url = options.payload.repository.ssh_url;
-            const repo_path = path.resolve(resolveHome(config.deploy_url), `${options.payload.repository.name}-${options.payload.repository.id}`);
-            child_process.exec(`git clone ${ssh_url} ${repo_path}`, () => {
-                console.log("Cloned")
-            })
+
+            this.updateRepo(options.payload.repository)
         })
+
+        this.addWebhook("create", (options) => {
+            this.updateRepo(options.payload.repository)
+        })
+    }
+
+    private updateRepo (repository: Repository) {
+        const default_branch = repository.default_branch
+        const ssh_url = repository.ssh_url;
+        const repo_path = path.resolve(resolveHome(config.deploy_url), `${repository.name}-${repository.id}`);
+
+        if (!fs.existsSync(repo_path)) {
+            child_process.exec(`git clone '${ssh_url}' '${repo_path}'`, (error) => {
+                console.log(error)
+                console.log(`Repository ${repository.full_name} cloned to ${repo_path}, deploying...`)
+
+                this.parseConfig(repo_path)
+            })
+        } else {
+            child_process.exec(`git -C '${repo_path}' reset --hard`, () => {
+                child_process.exec(`git clean -n -f ${repo_path}`, () => {
+                    child_process.exec(`git -C '${repo_path}' pull`, () => {
+                        console.log(`Pulled new changes from ${repository.full_name}, redeploying...`)
+
+                        this.parseConfig(repo_path)
+                    })
+                })
+            })
+        }
+    }
+
+    private parseConfig(repo_path: string) {
+        DeployConfig.parse(repo_path)
     }
 }
